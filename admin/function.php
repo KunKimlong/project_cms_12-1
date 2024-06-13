7e1606b628dbf18001b1ab3313b26b85b2dee4a3
KunKimlong <kimlongkun2412@gmail.com>
2024-06-13 13:40:18 +0700

    day 5

--- admin/function.php
+++ admin/function.php
@@ -197,3 +197,174 @@
         }
     }
     deleteLogo();
+
+
+    function addNews(){
+        global $connection;
+        if(isset($_POST['btn-add-news'])){
+            $title = $_POST['title'];
+            $news_type = $_POST['news_type'];
+            $category  = $_POST['category'];
+            $thumbnail = $_FILES['thumbnail']['name'];
+            $banner    = $_FILES['banner']['name'];
+            $description = $_POST['description'];
+
+            if(!empty($title) && !empty($news_type) && !empty($category) && !empty($thumbnail) && !empty($banner) && !empty($description)){
+                $thumbnail = rand(1,99999).'_'.$thumbnail;
+                $banner    = rand(1,99999).'_'.$banner;
+                $pathThumbanil = 'assets/image/'.$thumbnail;
+                $pathBanner = 'assets/image/'.$banner;
+
+                move_uploaded_file($_FILES['thumbnail']['tmp_name'],$pathThumbanil);
+                move_uploaded_file($_FILES['banner']['tmp_name'],$pathBanner);
+
+                $sql = "INSERT INTO `news`(`title`, `thumbnail`, `banner`, `news_type`, `category`, `description`) 
+                                    VALUES ('$title','$thumbnail','$banner','$news_type','$category','$description')";
+
+                $rs  = $connection->query($sql);
+                if($rs){
+                    echo '
+                        <script>
+                            $(document).ready(function(){
+                                swal({
+                                title: "Success",
+                                text: "News Insert successfully",
+                                icon: "success",
+                                });
+                            });
+                        </script>
+                    '; 
+                }
+            }
+            else{
+                echo '
+                        <script>
+                            $(document).ready(function(){
+                                swal({
+                                title: "error",
+                                text: "News can not Insert",
+                                icon: "error",
+                                });
+                            });
+                        </script>
+                    '; 
+            }
+        }
+    }
+    addNews();
+
+    function viewNews(){
+        global $connection;
+        $sql = "SELECT * FROM `news` ORDER BY `id` DESC";
+        $rs  = $connection->query($sql);
+        while($row = mysqli_fetch_assoc($rs)){
+            echo '
+                <tr>
+                    <td>'.$row['title'].'</td>
+                    <td>'.$row['news_type'].'</td>
+                    <td>'.$row['category'].'</td>
+                    <td><img src="assets/image/'.$row['thumbnail'].'" width="120px"/></td>
+                    <td><img src="assets/image/'.$row['banner'].'" width="120px"/></td>
+                    <td>'.$row['viewer'].'</td>
+                    <td>'.$row['postdate'].'</td>
+                    <td width="150px">
+                        <a href="update-news.php?id='.$row['id'].'" class="btn btn-primary">Update</a>
+                        <button type="button" remove-id="'.$row['id'].'" class="btn btn-danger btn-remove" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Remove
+                        </button>
+                    </td>
+                </tr>
+            ';
+        }
+    }
+
+    function updateNews(){
+        global $connection;
+        if(isset($_POST['btn-update-news'])){
+            $id  =  $_GET['id']; 
+            $title = $_POST['title'];
+            $news_type = $_POST['news_type'];
+            $category  = $_POST['category'];
+            $thumbnail = $_FILES['thumbnail']['name'];
+            $banner    = $_FILES['banner']['name'];
+            $description = $_POST['description'];
+
+            if($thumbnail){
+                $thumbnail = rand(1,99999).'_'.$thumbnail;
+                $pathThumbanil = 'assets/image/'.$thumbnail;
+                move_uploaded_file($_FILES['thumbnail']['tmp_name'],$pathThumbanil);
+            }
+            else{
+                $thumbnail = $_POST['old_thumbnail'];
+            }
+
+            if($banner){
+                $banner = rand(1,99999).'_'.$banner;
+                $pathBanner = 'assets/image/'.$banner;
+                move_uploaded_file($_FILES['banner']['tmp_name'],$pathBanner);
+            }
+            else{
+                $banner = $_POST['old_banner'];
+            }
+
+
+            if(!empty($title) && !empty($news_type) && !empty($category) && !empty($thumbnail) && !empty($banner) && !empty($description)){
+
+                $sql = "UPDATE `news` 
+                        SET `title`='$title',`thumbnail`='$thumbnail',`banner`='$banner',`news_type`='$news_type',`category`='$category',`description`='$description' 
+                        WHERE `id` = '$id'";
+                $rs  = $connection->query($sql);
+                if($rs){
+                    echo '
+                        <script>
+                            $(document).ready(function(){
+                                swal({
+                                title: "Success",
+                                text: "News Update successfully",
+                                icon: "success",
+                                });
+                            });
+                        </script>
+                    '; 
+                }
+
+            }
+            else{
+                echo '
+                        <script>
+                            $(document).ready(function(){
+                                swal({
+                                title: "Error",
+                                text: "News Update not success",
+                                icon: "error",
+                                });
+                            });
+                        </script>
+                    '; 
+            }
+        }
+    }
+    updateNews();
+
+    function deleteNews(){
+        global $connection;
+        if(isset($_POST['btn-delete-news'])){
+            $id = $_POST['remove_id'];
+            $sql = "DELETE FROM `news` WHERE `id`='$id'";
+            $rs  = $connection->query($sql);
+            if($rs){
+                echo '
+                <script>
+                    $(document).ready(function(){
+                        swal({
+                        title: "Success",
+                        text: "News Delete successfully",
+                        icon: "success",
+                        });
+                    });
+                </script>
+            '; 
+            }
+        }
+    }
+    deleteNews();
